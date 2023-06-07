2752fa095384824a754df616cc307a11dfdfcc20
lucacicada <100947658+lucacicada@users.noreply.github.com>
2023-06-07 09:17:25 +0200

    chore: switch to unbuild

---
+++ build.config.ts
@@ -0,0 +1,18 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  declaration: true,
+  entries: [
+    './src/index',
+    {
+      builder: 'mkdist',
+      input: './src/mailers/mailgun/',
+      outDir: './dist/mailers/mailgun/',
+    },
+    {
+      builder: 'mkdist',
+      input: './src/mailers/ses/',
+      outDir: './dist/mailers/ses/',
+    },
+  ],
+})
